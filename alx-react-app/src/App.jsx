dec20b5a2e6df56c5bbfb8c3ef772fa7980183fe
Yeabtsega Kefelegn <kzyeabtsega@gmail.com>
2025-07-20 16:14:46 +0300

    props used

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Yeabtsega" age= "21" bio="hello everyone!"/>
     </>
   )
 }

747dc8d932ec2259ab1edd30fc8b3816ad4c8f9a
Yeabtsega Kefelegn <kzyeabtsega@gmail.com>
2025-07-27 13:32:02 +0300

    prop drilling

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,7 +6,7 @@ export default function Counter() {
         <div>
             <p>Current Count: {count}</p>
             <button onClick = {() => setCount(count + 1)}>Increment</button>
-            <button onClick = {() => setCount(count - 1)}>Decreement</button>
+            <button onClick = {() => setCount(count - 1)}>Decrement</button>
             <button onClick = {() => setCount(0)}>Reset</button>
         </div>
     )

9137d7fdd1191393ad74b65afde4366dd8b1b9a4
Yeabtsega Kefelegn <kzyeabtsega@gmail.com>
2025-08-15 22:51:16 +0300

    className added

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,11 +14,11 @@ export default function RecipeList(){
         <div class="grid lg:grid-cols-4 md:grid-cols-3 gap-6 sm:grid-cols-2 bg-green-300 p-8 m-7">
             
             {recipes.map((recipe)=>(
-                <div key={recipe.id} class="p-2 bg-green-500 shadow-lg rounded-lg hover:scale-110 transition-transform duration-300 hover:shadow-[4px_4px_10px_black] transition-shadow duration-300 ">
-                    <img src={recipe.image } class="shadow-[6px_4px_10px] rounded-full mx-auto mb-5 mt-5 w-60 h-60  hover:scale-105 transition-transform duration-300 hover:rotate-180 transition transform duration-200 " ></img>
+                <div key={recipe.id} className="p-2 bg-green-500 shadow-lg rounded-lg hover:scale-110 transition-transform duration-300 hover:shadow-[4px_4px_10px_black] transition-shadow duration-300 ">
+                    <img src={recipe.image } className="shadow-[6px_4px_10px] rounded-full mx-auto mb-5 mt-5 w-60 h-60  hover:scale-105 transition-transform duration-300 hover:rotate-180 transition transform duration-200 " ></img>
                     <div>
-                        <h2 class=" text-xl font-bold  text-center text-white mb-5 ">{recipe.title}</h2>
-                        <p class="text-white text-center mb-5">{recipe.summary}</p>
+                        <h2 className=" text-xl font-bold  text-center text-white mb-5 ">{recipe.title}</h2>
+                        <p className="text-white text-center mb-5">{recipe.summary}</p>
                     </div>
                 </div>
             

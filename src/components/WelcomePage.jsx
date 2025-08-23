42a141557741aa054171af8954fdccf09f62b7f7
Yeabtsega Kefelegn <kzyeabtsega@gmail.com>
2025-08-23 17:11:43 +0300

    The welcome page styled by tailwind from css

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -9,19 +9,20 @@ function WelcomePage() {
   };
 
   return (
-    <div className="welcome-container">
-      <div className="welcome-content">
-        <h1 className="welcome-title">CashFlip</h1>
-        <p className="welcome-subtitle">Convert currencies in real-time</p>
-        <button onClick={handleStart} className="welcome-button">
+    <div className = "min-h-screen flex flex-col justify-center items-center bg-blue-200 text-center px-5 m-0 relative">
+      <div className="flex flex-col justify-center items-center max-w-3x1 w-full">
+        <h1 className=" text-black lg:text-[7rem] md:text-[4.5rem] sm:text-[4rem] text-[4rem] font-bold font-sans" >CashFlip</h1>
+        <p className=" px-2 text-sm lg:text-lg mb-8  text-black max-w-md sm:text-sm text-[1rem] font-sans">Convert currencies in real-time</p>
+        <button onClick={handleStart} className=" font-semibold bg-blue-600 px-3 py-1 sm:px-5 sm:py-2 md:px-8 md:py-3 lg:px-16 lg:py-4 lg:text-[2.5rem] md:text-[2rem] text-[2.5rem] text-white rounded-full hover:bg-blue-800 hover:translate-y-1 hover:shadow-xl ">
           Get Started
         </button>
       </div>
-      <footer className="welcome-footer">
+      <footer className= " absolute bottom-5 w-full text-center text-gray-800 font-bold text-[0.8rem] lg:text-[1rem] font-sans md:text-[0.9rem] sm:text-[0.7rem] ">
         <p>Powered by Egele</p>
       </footer>
     </div>
   );
+
 }
 
 
